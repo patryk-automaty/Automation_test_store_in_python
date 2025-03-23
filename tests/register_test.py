d8e8145fc8bcc813d2514a2c25d751c0f8e104d0
patryk <patkycautomaty@gmail.com>
2025-03-23 08:33:52 +0000

    add comments in register test and update user data

--- tests/register_test.py
+++ tests/register_test.py
@@ -13,11 +13,6 @@ from utils.helper_tools import HelperTools
 
 
 class RegisterTest(unittest.TestCase):
-    first_name = "Pat"
-    last_name = "Kat"
-    email = "123@123.pl"
-    telephone = "123123123"
-    fax = "123123123"
 
     @classmethod
     def setUpClass(cls):
@@ -38,15 +33,18 @@ class RegisterTest(unittest.TestCase):
     # tc 1
     def test_register_user_with_valid_data(self):
 
+        # Initialize helper tool for test data generation
         helper_tool = HelperTools()
 
+        # Generate user data and save for later use
         generated_data = helper_tool.register_user_data_generator()
         helper_tool.save_test_data_to_json(generated_data)
 
+        # Retrieve the last saved data
         user_data = helper_tool.get_last_saved_data()
 
 
-        # define data for test
+        # define address details for test
         address1 = "Krucza 1/2"
         address2 = "apartament 5"
         city = "Warsaw"
@@ -54,12 +52,16 @@ class RegisterTest(unittest.TestCase):
         zipcode = "01-100"
         country = "Poland"
 
-        # define expected results for assertion
+        # Define expected results for assertions
         expected_success_register_text = "YOUR ACCOUNT HAS BEEN CREATED!"
-        expected_logout_header_text = 'LOGOUT PAGE'
+        expected_logout_header_text = 'ACCOUNT LOGOUT'
+        expected_my_account_header_text = 'MY ACCOUNT'
 
+        # Navigate to login page and proceed with registration
         self.home_page.go_to_login_page()
         self.login_page.click_continue()
+
+        # Input personal details from generated test data
         self.register_page.input_personal_details(
             user_data["first_name"],
             user_data["last_name"],
@@ -68,7 +70,7 @@ class RegisterTest(unittest.TestCase):
             user_data["fax"]
         )
 
-
+        # Input address details
         self.register_page.input_address(
             company="company",
             address1=address1,
@@ -79,29 +81,43 @@ class RegisterTest(unittest.TestCase):
             zipcode=zipcode
         )
 
+        # Input login details from generated data
         self.register_page.login_details(
             login_name=user_data["login_name"],
             password=user_data["password"],
             confirm_password=user_data["password"]
         )
 
+        # Select newsletter and agree privacy policy
         self.register_page.newsletter_and_privacy_policy(
             subscribe=True,
             privacy_policy=True
         )
 
+        # Click continue to finish registration
         self.register_page.click_continue()
 
+        # Verify successful registration message
         actual_success_register_text = self.register_success_page.get_register_success_header_text()
         self.assertIn(expected_success_register_text, actual_success_register_text)
 
+        # Click continue button and redirect to my account page
         self.register_success_page.click_continue_button()
 
+        # Log out from account
         self.my_account_page.logout_from_my_account_box()
+
+        # Verify logout confirmation message
+        actual_logout_header_text = self.logout_page.get_account_logout_header_text()
+        self.assertIn(expected_logout_header_text, actual_logout_header_text)
+
+        # Click continue after logout
         self.logout_page.click_continue()
+
+        # Navigate to login page and login to registered credentials
         self.home_page.go_to_login_page()
-
-    #    actual_logout_header_text = self.logout_page.get_account_logout_header_text()
-     #   self.assertIn(expected_logout_header_text, actual_logout_header_text)
-
         self.login_page.login_to_account(user_data["login_name"], user_data["password"])
+
+        # Verify login was successful and my account page is displayed
+        actual_my_account_header_text = self.my_account_page.get_my_account_header_text()
+        self.assertIn(expected_my_account_header_text, actual_my_account_header_text)
