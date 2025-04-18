f2ced1f63055096914d41d2c7232120643872d53
patryk <patkycautomaty@gmail.com>
2025-04-18 11:27:42 +0000

    update driver_factory.py to CI

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -5,18 +5,37 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+import os
 
 
 def get_driver(browser="chrome"):
 
     if browser.lower() == "chrome":
         chrome_options = ChromeOptions()
-        chrome_options.add_argument("--start-maximized")  # Maximize window
+
+        if os.getenv("CI"):
+            chrome_options.add_argument("--headless=new")
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument("--window-size=1920,1080")
+        else:
+            chrome_options.add_argument("--start-maximized")
+
         service = ChromeService(ChromeDriverManager().install())  # Use ChromeService
         driver = webdriver.Chrome(service=service, options=chrome_options)
 
     elif browser.lower() == "firefox":
         firefox_options = FirefoxOptions()
+
+        if os.getenv("CI"):
+            firefox_options.add_argument("--headless")
+            firefox_options.add_argument("--no-sandbox")
+            firefox_options.add_argument("--disable-dev-shm-usage")
+            firefox_options.add_argument("--window-size=1920,1080")
+        else:
+            firefox_options.add_argument("--start-maximized")
+
         service = FirefoxService(GeckoDriverManager().install())  # Use FirefoxService
         driver = webdriver.Firefox(service=service, options=firefox_options)
 
